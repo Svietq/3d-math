03123abc6ccdfa4ef4a53338dbc56f51a141c613
Michał Świętek <mswietku@gmail.com>
2018-10-28 20:01:00 +0100

    Create a function for calculating cross product

--- 3d-math/main.cpp
+++ 3d-math/main.cpp
@@ -31,13 +31,26 @@ Scalar dot_product(const Vec & vec1, const Vec & vec2)
 	return vec1[X] * vec2[X] + vec1[Y] * vec2[Y] + vec1[Z] * vec2[Z];
 }
 
+Scalar coeff(Scalar a1, Scalar a2, Scalar b1, Scalar b2)
+{
+	return a1 * b2 - a2 * b1;
+}
+
+Vec cross_product(const Vec & vec1, const Vec & vec2)
+{
+	return Vec{ coeff(vec1[Y], vec1[Z], vec2[Y], vec2[Z]),
+			   -coeff(vec1[X], vec1[Z], vec2[X], vec2[Z]),
+				coeff(vec1[X], vec1[Y], vec2[X], vec2[Y]) };
+}
+
 int main() 
 {
 	Vec vec1{ 1, 2, 3 };
-	Vec vec2{ 3, 4, 5 };
+	Vec vec2{ 4, 5, 6 };
 
 	print(add(vec1, vec2));
 	print(dot_product(vec1, vec2));
+	print(cross_product(vec1, vec2));
 
 	std::cin.get();
 }
