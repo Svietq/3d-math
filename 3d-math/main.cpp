906df785e12b9dc0baee0446dbb8bbb579b4a6c6
Michał Świętek <mswietku@gmail.com>
2018-10-28 19:46:47 +0100

    Create a function for calculating dot product

--- 3d-math/main.cpp
+++ 3d-math/main.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 
 using Vec = std::vector<double>;
+using Scalar = double;
 enum {X = 0, Y = 1, Z = 2};
 
 void print(const Vec & vec)
@@ -14,10 +15,20 @@ void print(const Vec & vec)
 	std::cout << '\n';
 }
 
+void print(Scalar num)
+{
+	std::cout << num << '\n';
+}
+
 Vec add(const Vec & vec1, const Vec & vec2)
 {
 	auto vec = Vec{ vec1[X] + vec2[X], vec1[Y] + vec2[Y], vec1[Z] + vec2[Z] };
 	return vec;
+}
+
+Scalar dot_product(const Vec & vec1, const Vec & vec2)
+{
+	return vec1[X] * vec2[X] + vec1[Y] * vec2[Y] + vec1[Z] * vec2[Z];
 }
 
 int main() 
@@ -26,6 +37,7 @@ int main()
 	Vec vec2{ 3, 4, 5 };
 
 	print(add(vec1, vec2));
+	print(dot_product(vec1, vec2));
 
 	std::cin.get();
 }
